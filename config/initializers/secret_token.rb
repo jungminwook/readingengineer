2abd8b22b86878401e378a13904a891551e4c57d
jungminwook <jungminwook@gmail.com>
2013-11-09 18:29:32 +0900

    reading engineer templetes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Readingengineer::Application.config.secret_key_base = 'e7604973d176ab11ad251df2dd5963c1710dd2355b034aca386bfe6376280f22d08cd28549d3a3ae7d0f78c4a00fe461c47452519232bba3d3e40c927a13c2b3'
